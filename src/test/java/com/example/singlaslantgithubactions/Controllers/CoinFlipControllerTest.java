dee8167e0eab677253ca5b5b2fbd4c2086c2101a
Maria Hagelin <maria.hagelin@yh.nackademin.se>
2023-09-27 15:12:00 +0200

    testFlipCoin()

--- src/test/java/com/example/singlaslantgithubactions/Controllers/CoinFlipControllerTest.java
+++ src/test/java/com/example/singlaslantgithubactions/Controllers/CoinFlipControllerTest.java
@@ -3,6 +3,9 @@ package com.example.singlaslantgithubactions.Controllers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.ui.Model;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CoinFlipControllerTest {
@@ -16,12 +19,31 @@ class CoinFlipControllerTest {
     }
     @Test
     void calculateWinRateStartsWith0() {
-        assertTrue(coinFlipController.calculateWinRate()==100);
+        assertTrue(coinFlipController.calculateWinRate()==0);
     }
+
+    @Test
+    public void testFlipCoin() {
+        // Create a mock Model object using Mockito
+        Model model = Mockito.mock(Model.class);
+
+        // Call the controller method with the mock Model
+        String result = coinFlipController.flipCoin("heads", model);
+
+        // Perform assertions as needed
+        // You can verify interactions with the mock Model if necessary
+        Mockito.verify(model).addAttribute("rounds", Mockito.anyList());
+        Mockito.verify(model).addAttribute("WINNER", Mockito.anyString());
+
+        // Assert the return value (e.g., the redirect URL)
+        assertEquals("redirect:/", result);
+    }
+
     @Test
     void calculateWinRateAfterRoundIsLessThan100() {
-     //   coinFlipController.flipCoin("heads", Model model);
         assertTrue(coinFlipController.calculateWinRate()<0);
+        //   coinFlipController.flipCoin("heads", Model model);
+        //ska fela tills vidare
     }
 
 
