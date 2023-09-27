9e0d689a55def71d6e54d3546bc207ed79f74c06
Maria Hagelin <maria.hagelin@yh.nackademin.se>
2023-09-27 13:35:11 +0200

    Initial commit

---
+++ src/main/java/com/example/singlaslantgithubactions/SinglaSlantGitHubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.singlaslantgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinglaSlantGitHubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinglaSlantGitHubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/singlaslantgithubactions/SinglaSlantGitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.singlaslantgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinglaSlantGitHubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
