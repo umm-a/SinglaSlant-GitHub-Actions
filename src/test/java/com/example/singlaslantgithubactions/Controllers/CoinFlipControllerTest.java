42aa9b7be376f55496e64f0b972154b94ee6d1fd
Ling Wang <amyling.wang@gmail.com>
2023-09-27 23:14:20 +0200

    add 1 integration test in CoinFlipControllerTest

--- src/test/java/com/example/singlaslantgithubactions/Controllers/CoinFlipControllerTest.java
+++ src/test/java/com/example/singlaslantgithubactions/Controllers/CoinFlipControllerTest.java
@@ -3,15 +3,37 @@ package com.example.singlaslantgithubactions.Controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.ui.Model;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class CoinFlipControllerTest {
     CoinFlipController coinFlipController;
+    @Autowired
+    private MockMvc mockMvc;
     @BeforeEach
     void setUp(){
          coinFlipController = new CoinFlipController();
+    }
+
+    @Test
+    public void testPageIsDisplayedCorrectly() throws Exception{
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("index"))
+                .andExpect(content().string(containsString("Coin Flip Game")));
     }
     @Test
     void flipCoin() {
